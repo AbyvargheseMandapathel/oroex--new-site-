a6592e42d2d0b239b3bf14934414695828186850
AbyvargheseMandapathel <abyvarghesemandapathel@gmail.com>
2025-11-23 21:02:27 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
   ],
+  server: {
+    host: true, // required for ngrok / external access
+    allowedHosts: [
+      "976e6fa772be.ngrok-free.app"  // your ngrok URL
+    ]
+  }
 })
