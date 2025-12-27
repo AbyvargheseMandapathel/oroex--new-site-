6726b4b2336cd588c34c536521a06590d08ddd78
AbyvargheseMandapathel <abyvarghesemandapathel@gmail.com>
2025-12-28 00:20:21 +0530

    test

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,6 @@
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8001/api';
+// const BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8001/api';
+
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://69.62.77.40:8001/api';
 
 export const getNavbars = async () => {
     const response = await fetch(`${BASE_URL}/navbars/`);
